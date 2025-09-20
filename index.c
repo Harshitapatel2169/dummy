b215c3d20e5c9dc666ccddcd7955a7eda3e1db4a
Harshitapatel2169 <harshitapatel9202@gmail.com>
2025-09-20 12:46:30 +0530

    Add files via upload

---
+++ index.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int age=20;
+    float pi=3.14;
+    char grade='A';
+    printf("hello students!\n");
+    printf("age=%d\n",age);
+    printf("Pi=%f\n",pi);
+    printf("Grade=%c\n",grade);
+    return 0;
+}
