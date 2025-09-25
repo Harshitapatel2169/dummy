10ba3e40560d45f1e81b6e62782ef9dadecb6576
Harshitapatel2169 <harshitapatel9202@gmail.com>
2025-09-25 13:53:12 +0530

    Add files via upload

---
+++ lect4.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+int main(){
+//     int age=24;
+//     float pi=3.23;
+//     char grade='A';
+//     printf("hello students!\n");
+//     printf("Age=%d\n",age);
+//     printf("Pi=%f\n",pi);
+//     printf("Grade=%c\n",grade);
+//     return 0;
+// int marks=90;
+// if(marks>=87){
+// printf("Grade:A\n");
+// }
+// else if(marks>=60){
+//     printf("Grade:B\n");
+// }
+// else {
+//     printf("fail\nu");
+// }
+// int amount;
+// printf("Enter amount in indian..");
+// scanf("%d",&amount);
+// char ch;
+// printf("which you want to convert");
+// scanf("%c",&ch);
+// switch(ch)
+// {
+//     case'$':
+//     printf("%f",(float)amount/87)
+//     break;
+//     case''
+//     0.87==1/87
+//     default:
+//     break;
+// }
+
+// int a=4;
+// switch()
+// {
+// case1:
+// printf("hello");
+// break;
+// printf("hiii");
+// break;
+// default:
+// printf("kuch bhi nai");
+// break;
+// }
+// a<5?printf("hello"):printf("hiii");
+int a=5;
+if(a<5){
+    printf("hello");
+    printf("byee");
+}
+else 
+printf("hii");
+
+}
+
+
