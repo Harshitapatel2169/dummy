61c0df630a3646adc4935935d1d6c5ae2eb9afbc
Harshitapatel2169 <harshitapatel9202@gmail.com>
2025-10-05 13:43:03 +0530

    Add files via upload

---
+++ lect9.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main() {
+    // for(int i=1;i<4;i++){
+    //     for(int j=1;j<=2*(4-i);j++){
+    //         printf("*");
+    //     }
+    //     printf("\n");
+    // }
+for(int i=0;i<4;i++)
+{
+for(int j=0;j<i;j++)
+{
+    printf("*");
+}
+for(int k=4;k>i;k--)
+{
+    printf(" ");
+}
+for(int p=4;p>i;p--)
+{
+    printf(" ");
+}
+for(int r=0;r<=i;r++){
+    printf("*");
+}
+printf("\n");
+}
+for(int i=0;i<4;i++){
+    for(int j=0;j<=i;j++){
+printf(" ");
+    }
+    for(int k=3;k>i;k--){
+        printf("*");
+
+    }
+    for(int p=3;p>=i;p--){
+        printf("*");
+    }
+    printf("\n");
+}
+}
